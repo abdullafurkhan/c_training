a8cd4e9cd07e72bfcc415c6060bfc576845e416b
abdullafurkhan <abdullafurkhan95@gmail.com>
2025-11-22 10:40:04 +0530

    Add files via upload

---
+++ call_ref_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+struct score
+{
+    float marks;
+    int rank;
+};
+void edit(struct score *s1)
+{
+    printf("before change in fn : %f\n", s1->marks);
+    s1->marks = 77.9;
+    printf("after change in fn : %f\n", s1->marks);
+}
+void main()
+{
+    struct score s1 = {55.9, 2};
+    edit(&s1);
+    printf("after change in main %f\n", s1.marks);
+}
